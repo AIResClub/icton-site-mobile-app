8544b32488fc9ebe9121ac1c43c8aa623a114997
Wiaci <vyachproz7@gmail.com>
2021-05-09 04:30:58 +0300

    ICTclubs developed

--- app/src/main/java/com/itmo/ictmobile/util/TestData.kt
+++ app/src/main/java/com/itmo/ictmobile/util/TestData.kt
@@ -7,9 +7,24 @@ object ClubsTestData {
     fun get(): List<Club> {
         return listOf(
             Club(
-                "Проектный клуб факультета ИКТ Info Lab",
+                "Info Lab",
                 R.raw.club_infolab,
-            "https://vk.com/infolab_club"
+                "https://vk.com/infolab_club"
+            ),
+            Club(
+                "AIRes",
+                R.raw.club_aires,
+                "https://vk.com/airesclub"
+            ),
+            Club(
+                "DesArm",
+                R.raw.club_desarm,
+                "https://vk.com/desarm_itmo"
+            ),
+            Club(
+                "ITMeladze",
+                R.raw.club_meladze,
+                "https://vk.com/itmeladze"
             )
         )
     }
